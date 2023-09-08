a28b262b2c78c7ea53ee06be046ae216b4599743
Vinci Wu <wu.vinci00@gmail.com>
2023-09-08 17:31:38 -0400

    Defined a skeleton implementation for an OS shell run and exit functions

---
+++ src/Shell.cpp
@@ -0,0 +1,26 @@
+#include "Shell.h"
+
+Shell::Shell()
+{
+
+}
+
+void Shell::runShell()
+{
+	std::string user_input;
+	while (1)
+	{
+		std::cout << "oddOS$ ";
+		std::getline(std::cin, user_input);
+		if (user_input == "exit")
+		{
+			endShell();
+		}
+		std::cout << user_input << std::endl;
+	}
+}
+
+void Shell::exitShell()
+{
+	std::exit(EXIT_SUCCESS);
+}
