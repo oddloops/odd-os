12f05a14f5122fd96b7d370f3a337c64a1bb6112
Vinci Wu <wu.vinci00@gmail.com>
2023-09-08 16:23:31 -0400

    Created a loop to read user inputs and break if the user input inputs break

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-	std::cout << "New Project" << std::endl;
+	std::string user_input;
+	while (1)
+	{
+		std::cout << "oddOS$ ";
+		std::getline(std::cin, user_input);
+
+		if (user_input == "exit")
+		{
+			break;
+		}
+		std::cout << user_input << std::endl;
+	}
 	return 0;
 }
