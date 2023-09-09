e72b7be4086a5dbdaf9d6a798532aef362b45ed8
Vinci Wu <wu.vinci00@gmail.com>
2023-09-09 18:10:15 -0400

    Created implicit declarations for parsing input, executing commands, and helper functions. Made every method private other than runShell

--- include/Shell.h
+++ include/Shell.h
@@ -2,14 +2,31 @@
 #define SHELL_H
 
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 class Shell
 {
 public:
-	Shell();
+	// Runs the shell emulation
 	void runShell();
+private:
+	// Exits the shell emulation
 	void exitShell();
+
+	// Input: Takes user inputted string
+	// Output: user inputted string as a vector
+	// - 0: Command
+	// - 1+: Arguments
+	std::vector<std::string> parseInput(const std::string&);
+
+	// Run the command with its given arguments
+	void executeCommand(const std::vector<std::string>&);
+
+	// Helper Functions
+	// Convert a string vector to a C string array
+	const char** convertToCStringArray(const std::vector<std::string>&);
 };
 
 #endif // !SHELL_H
