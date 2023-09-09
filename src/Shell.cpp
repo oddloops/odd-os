0149e1afd6b2251e94ed4cfda51254f4aafbaef1
Vinci Wu <wu.vinci00@gmail.com>
2023-09-09 18:11:29 -0400

    Implemented parseInput method and convertToCStringArray helper method

--- src/Shell.cpp
+++ src/Shell.cpp
@@ -1,10 +1,5 @@
 #include "Shell.h"
 
-Shell::Shell()
-{
-
-}
-
 void Shell::runShell()
 {
 	std::string user_input;
@@ -12,15 +7,56 @@ void Shell::runShell()
 	{
 		std::cout << "oddOS$ ";
 		std::getline(std::cin, user_input);
-		if (user_input == "exit")
+		std::vector<std::string> input = parseInput(user_input);
+		for (std::string s : input)
+		{
+			std::cout << s << std::endl;
+		}
+		if (input[0] == "exit")
 		{
 			exitShell();
 		}
-		std::cout << user_input << std::endl;
 	}
 }
 
 void Shell::exitShell()
 {
 	std::exit(EXIT_SUCCESS);
+}
+
+std::vector<std::string> Shell::parseInput(const std::string& user_input)
+{
+	std::istringstream iss(user_input);
+	std::vector<std::string> tokens;
+	std::string token;
+
+	while (std::getline(iss, token, ' '))
+	{
+		tokens.push_back(token);
+	}
+	return tokens;
+}
+
+void executeCommand(const std::vector<std::string>& command)
+{
+	if (command.size() == 0)
+	{
+		return;
+	}
+	if (command[0] == "exit")
+	{
+		return;
+	}
+}
+
+const char** convertToCStringArray(const std::vector<std::string>& tokens)
+{
+	size_t n = tokens.size();
+	const char** args = new const char* [n + 1];
+	for (size_t i = 0; i < n; i++)
+	{
+		args[i] = tokens[i].c_str();
+	}
+	args[n] = nullptr;
+	return args;
 }
